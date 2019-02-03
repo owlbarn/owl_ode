a9f6027e375be381357b9ec2daf2149550f6b953
Marcello Seri <marcello.seri@gmail.com>
2019-02-03 13:20:15 +0100

    sundials: add some comments to think about
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/sundials.ml
+++ src/sundials.ml
@@ -7,6 +7,9 @@ let unwrap (dim1, dim2) x =
   genarray_of_array2 (reshape_2 (genarray_of_array1 x) dim1 dim2)
 
 let odeint ?(stiff=false) ~f ?(t0=0.) ~y0 =
+  (* Maybe this kind of checks should go to Common -- with an odeint equivalent
+     and be used everywhere. We will just pass the step function to be used in
+     the integration loop: that one will take the ~f parameter. *)
   let dim1, dim2 = Mat.shape y0 in
   assert ((Mat.shape (f t0 y0)) = (dim1, dim2));
   (* rhs function that sundials understands *)
@@ -21,7 +24,11 @@ let odeint ?(stiff=false) ~f ?(t0=0.) ~y0 =
     | true  -> Cvode.(init BDF   Functional default_tolerances f_wrapped t0 yvec) in
   fun ~dt ~duration () ->
     Cvode.set_stop_time session duration;
-    let n_steps = int_of_float (duration /. dt) in
+    let n_steps = Common.steps t0 (t0 +. duration) dt in
+    (* XXX: just food for thoughts:
+       all sizes are known at this point, it may be better to create the
+       arrays and fill them in sequentially instead. And maybe use Owl.Arr as
+       containers instead of Array (like in the ocaml impls). *)
     let rec run i tacc yacc = 
       if i < n_steps then begin
         let t = dt *. float i in
