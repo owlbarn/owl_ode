fad6fe86f9dedee396d223df294bacc4809222e4
Marcello Seri <marcello.seri@gmail.com>
2019-02-03 13:06:07 +0100

    ocaml integrators: cleanup and simplify implementations
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/common.ml
+++ src/common.ml
@@ -1,2 +1,31 @@
 (* TODO: update implementations of multiple order RK on the line of
  * symplectic.ml *)
+
+type timespan = float * float
+(** Representation of a time span. *)  
+
+let steps t0 t1 dt =
+  (t1 -. t0)/.dt |> Float.floor |> int_of_float
+
+let integrate ~step y0 (t0, t1) dt =
+  let (.${}) = Owl.Mat.(.${}) in
+  let (.${}<-) = Owl.Mat.(.${}<-) in
+  (* get the slices indexes for xs and ys *)
+  let slices idx elts =
+    [[idx]; [0; elts/2-1]], [[idx]; [elts/2; elts-1]]
+  in
+  let steps = steps t0 t1 dt in
+  let _, elts = Owl.Mat.shape y0 in
+  let sol = Owl.Mat.empty steps elts in
+  let t = ref t0 in
+  sol.${[[0]]}<- y0;
+  for idx = 1 to steps-1 do
+    let xi, pi = slices (idx-1) elts in
+    let xi', pi' = slices idx elts in
+    let xs, ps = sol.${xi}, sol.${pi} in
+    let xs', ps', t' = step xs ps !t in
+    sol.${xi'}<- xs';
+    sol.${pi'}<- ps';
+    t := t';
+  done;
+  sol
--- src/symplectic.ml
+++ src/symplectic.ml
@@ -1,57 +1,59 @@
-type timespan = float * float
-(** Representation of a time span. *)  
-
-let steps t0 t1 dt =
-  (t1 -. t0)/.dt |> Float.floor |> int_of_float
-
-(** Private function, allow to get the slices indexes for xs and ys *)
-let slices idx elts =
-  [[idx]; [0; elts/2-1]], [[idx]; [elts/2; elts-1]]
-
-(* opening Owl.Mat or Owl.Arr messes up badly all the integer operations *)
-let (.${}) = Owl.Mat.(.${})
-let (.${}<-) = Owl.Mat.(.${}<-)
-
-let symplectic_euler ~f y0 (t0, t1) dt =
+let symplectic_euler ~f y0 tspan dt =
   let _, elts = Owl.Mat.shape y0 in
   assert (Owl.Maths.is_even elts);
-
-  let steps = steps t0 t1 dt in
-  let sol = Owl.Mat.empty steps elts in
-
-  sol.${[[0]]}<- y0;
-  for idx = 1 to steps-1 do
-    let xi, pi = slices (idx-1) elts in
-    let xi', pi' = slices idx elts in
-    let xs, ps = sol.${xi}, sol.${pi} in
-    let t = t0 +. dt *. (float_of_int idx) in
+  let step xs ps t0 =
+    let t = t0 +. dt in
     let fxs = f xs ps t in
     let ps' = Owl.Mat.(ps + fxs *$ dt) in
-    sol.${pi'}<- ps';
-    sol.${xi'}<- Owl.Mat.(xs + ps' *$ dt);
-  done;
-  sol
+    let xs' = Owl.Mat.(xs + ps' *$ dt) in
+    xs', ps', t
+  in
+  Common.integrate ~step y0 tspan dt
 
-let leapfrog ~f y0 (t0, t1) dt =
+let leapfrog ~f y0 tspan dt =
   let _, elts = Owl.Mat.shape y0 in
   assert (Owl.Maths.is_even elts);
-
-  let steps = steps t0 t1 dt in
-  let sol = Owl.Mat.empty steps elts in
-
-  sol.${[[0]]}<- y0;
-  for idx = 1 to steps-1 do
-    let xi, pi = slices (idx-1) elts in
-    let xi', pi' = slices idx elts in
-    let xs, ps = sol.${xi}, sol.${pi} in
-    let t = t0 +. dt *. (float_of_int idx) in
+  let step xs ps t0 =
+    let t = t0 +. dt in
     let fxs = f xs ps t in
     let xs' = Owl.Mat.(xs + ps *$ dt + fxs *$ (dt*.dt*.0.5)) in
     let fxs' = f xs' ps (t +. dt) in
-    sol.${xi'}<- xs';
-    sol.${pi'}<- Owl.Mat.(ps + (fxs + fxs') *$ (dt*.0.5));
-  done;
-  sol
+    let ps' = Owl.Mat.(ps + (fxs + fxs') *$ (dt*.0.5)) in
+    xs', ps', t
+  in
+  Common.integrate ~step y0 tspan dt
+
+
+(* For the values used in the implementations below
+   see Candy-Rozmus (https://www.sciencedirect.com/science/article/pii/002199919190299Z)
+   and https://en.wikipedia.org/wiki/Symplectic_integrator *)
+let symint ~coeffs ~f y0 tspan dt =
+  let _, elts = Owl.Mat.shape y0 in
+  assert (Owl.Maths.is_even elts);
+  let symint_step ~coeffs ~f xs ps t dt =
+    List.fold_left (fun (xs, ps, t) (ai, bi) ->
+        let ps' = Owl.Mat.(ps + (f xs ps t) *$ (dt*.bi)) in
+        let xs' = Owl.Mat.(xs + ps' *$ (dt *. ai)) in
+        let t = t +. dt*.ai in
+        (xs', ps', t))
+      (xs, ps, t)
+      coeffs
+  in
+  let step xs ps t = symint_step ~coeffs ~f xs ps t dt in
+  Common.integrate ~step y0 tspan dt
+
+let cleapfrog = [ (0.5, 0.0); (0.5, 1.0) ]
+let cpseudoleapfrog = [ (1.0, 0.5); (0.0, 0.5) ]
+let cruth3 = [ (2.0/.3.0, 7.0/.24.0); (-2.0/.3.0, 0.75); (1.0, -1.0/.24.0)]
+let cruth4 = let c = Owl.Maths.pow 2.0 (1.0/.3.0) in
+  [ (0.5, 0.0); (0.5*.(1.0-.c), 1.0); (0.5*.(1.0-.c), -.c); (0.5, 1.0)]
+  |> List.map (fun (v1,v2) -> v1 /. (2.0 -. c), (v2 /. (2.0 -. c)))
+
+let leapfrog' ~f y0 (t0, t1) dt = symint ~coeffs:cleapfrog ~f y0 (t0, t1) dt
+let pseudoleapfrog ~f y0 (t0, t1) dt = symint ~coeffs:cpseudoleapfrog ~f y0 (t0, t1) dt
+let ruth3 ~f y0 (t0, t1) dt = symint ~coeffs:cruth3 ~f y0 (t0, t1) dt
+let ruth4 ~f y0 (t0, t1) dt = symint ~coeffs:cruth4 ~f y0 (t0, t1) dt
+
 
 (*
     (* XXX: 
@@ -83,50 +85,3 @@ let leapfrog_implicit ~f y0 (t0, t1) dt =
    done;
    sol
 *)
-
-(* TODO: impl below is rushed up and definitely broken*)
-
-(* For the values used in the implementations below
-   see Candy-Rozmus (https://www.sciencedirect.com/science/article/pii/002199919190299Z)
-   and https://en.wikipedia.org/wiki/Symplectic_integrator *)
-
-let symint_step ~coeffs ~f xs ps t dt =
-  List.fold_left (fun (xs, ps, t) (ai, bi) ->
-      let ps' = Owl.Mat.(ps + (f xs ps t) *$ (dt*.bi)) in
-      let xs' = Owl.Mat.(xs + ps' *$ (dt *. ai)) in
-      let t = t +. dt*.ai in
-      (xs', ps', t))
-    (xs, ps, t)
-    coeffs
-
-let symint ~coeffs ~f y0 (t0, t1) dt =
-  let _, elts = Owl.Mat.shape y0 in
-  assert (Owl.Maths.is_even elts);
-
-  let steps = steps t0 t1 dt in
-  let sol = Owl.Mat.empty steps elts in
-
-  sol.${[[0]]}<- y0;
-  let t = ref t0 in
-  for idx = 1 to steps-1 do
-    let xi, pi = slices (idx-1) elts in
-    let xi', pi' = slices idx elts in
-    let xs, ps = sol.${xi}, sol.${pi} in
-    let xs', ps', t' = symint_step ~coeffs ~f xs ps !t dt in
-    t := t';
-    sol.${xi'}<- xs';
-    sol.${pi'}<- ps';
-  done;
-  sol
-
-let cleapfrog = [ (0.5, 0.0); (0.5, 1.0) ]
-let cpseudoleapfrog = [ (1.0, 0.5); (0.0, 0.5) ]
-let cruth3 = [ (2.0/.3.0, 7.0/.24.0); (-2.0/.3.0, 0.75); (1.0, -1.0/.24.0)]
-let cruth4 = let c = Owl.Maths.pow 2.0 (1.0/.3.0) in
-  [ (0.5, 0.0); (0.5*.(1.0-.c), 1.0); (0.5*.(1.0-.c), -.c); (0.5, 1.0)]
-  |> List.map (fun (v1,v2) -> v1 /. (2.0 -. c), (v2 /. (2.0 -. c)))
-
-let leapfrog' ~f y0 (t0, t1) dt = symint ~coeffs:cleapfrog ~f y0 (t0, t1) dt
-let pseudoleapfrog ~f y0 (t0, t1) dt = symint ~coeffs:cpseudoleapfrog ~f y0 (t0, t1) dt
-let ruth3 ~f y0 (t0, t1) dt = symint ~coeffs:cruth3 ~f y0 (t0, t1) dt
-let ruth4 ~f y0 (t0, t1) dt = symint ~coeffs:cruth4 ~f y0 (t0, t1) dt
