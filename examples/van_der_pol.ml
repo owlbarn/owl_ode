9b3ae4997478e261423a5c7b5dbc9c589574e810
Marcello Seri <marcello.seri@gmail.com>
2019-02-06 22:10:26 +0100

    van_der_pol: port to new interface
    Note: currently fails at
    
    let ts, ys = Ode.OdeSolver.odeint ~algo:(cvode ()) ~problem ~tspec () in
    
    with Fatal error: exception File src/owl/core/owl_slicing.ml, line 95, characters 2-8: Assertion failed
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- examples/van_der_pol.ml
+++ examples/van_der_pol.ml
@@ -1,11 +1,10 @@
 open Owl
-open Ode.Sundials
 
 let tau = 150E-3 
 let f = 
   let mu = 10. in
   let lambda = 0.1 in
-  fun _t y ->
+  fun y _t ->
     let y = Mat.to_array y in
     [|[| y.(1) |]; 
       [| -.y.(0) +. mu *. y.(1) *. (lambda -. Maths.sqr y.(0))|]|]
@@ -14,10 +13,11 @@ let f =
 let y0 = Mat.of_array [|0.02; 0.03|] 2 1
 
 let () = 
-  let ts, ys = odeint ~f ~dt:1E-2 ~duration:30. ~y0 () in
+  let open Ode.OdeSolver in
+  let problem = {f; y0} in
+  let tspec = Ode.T1 {t0=0.0; dt=1E-2; duration=30.0} in
+  let ts, ys = Ode.OdeSolver.odeint ~algo:(cvode ()) ~problem ~tspec () in
   (* save ts and ys *)
   let ts = [| ts |] |> Mat.of_arrays |> Mat.transpose in
-  let ys = ys |> Mat.concatenate ~axis:1 |> Mat.transpose in
-  Mat.save_txt Mat.(ts @|| ys) "van_der_pol_dynamics"
-
-
+  let ys = ys |> Mat.transpose in
+  Mat.save_txt Mat.(ts @|| ys) "van_der_pol_dynamics.txt"
