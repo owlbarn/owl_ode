abd7819befbc543c6dc865bb8a130ace5a1d0850
Marcello Seri <marcello.seri@gmail.com>
2019-02-07 17:04:59 +0100

    native: warn about the usage of RK45
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/ode/native.mli
+++ src/ode/native.mli
@@ -10,7 +10,7 @@ module RK4: Types.SolverT
   with type t = Owl.Mat.mat
    and type output = float array * Owl.Mat.mat
 
-(** Default tol = 1e-7 *)
+(** Default tol = 1e-7 -- untested, please do not rely on this *)
 module RK45: Types.SolverT
   with type t = Owl.Mat.mat
    and type output = float array * Owl.Mat.mat
