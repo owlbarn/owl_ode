2c15856da5b4bd0357f765749376cda4b9723c6e
Marcello Seri <marcello.seri@gmail.com>
2019-07-19 02:36:37 +0200

    expose stepper and integrator
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/odepack/owl_ode_odepack.mli
+++ src/odepack/owl_ode_odepack.mli
@@ -7,6 +7,24 @@ val unwrap
   -> (float, Bigarray.float64_elt, Bigarray.c_layout) Bigarray.Array1.t
   -> Owl.Mat.mat
 
+val lsoda_i
+  :  relative_tol:float
+  -> abs_tol:float
+  -> (Owl.Mat.mat -> float -> Owl.Mat.mat)
+  -> Owl.Mat.mat
+  -> Owl_ode.Types.tspec
+  -> unit
+  -> Owl.Mat.mat * Owl.Mat.mat
+
+val lsoda_s
+  :  relative_tol:float
+  -> abs_tol:float
+  -> (Owl.Mat.mat -> float -> Owl.Mat.mat)
+  -> dt:float
+  -> Owl.Mat.mat
+  -> float
+  -> Owl.Mat.mat * float
+
 val lsoda
   :  relative_tol:float
   -> abs_tol:float
